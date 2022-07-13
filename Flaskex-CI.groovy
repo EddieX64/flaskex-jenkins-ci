b0e9467f1d62b23801b3fb362bf4261a4af8ede3
EddieX64 <edik_rahimzhanov@mail.ru>
2022-07-13 18:38:14 +0600

    Initial commit

---
+++ Flaskex-CI.groovy
@@ -0,0 +1,24 @@
+pipeline {
+  agent {
+     node { 
+        label 'ec2-asg'
+        }
+    
+  }
+  options { timestamps () }
+  
+  stages {
+    stage('Cleanup WS') {
+      steps {
+        cleanWs()
+        git branch: '**', changelog: true, poll: false, url: 'https://github.com/EddieX64/flaskex.git' 
+      }
+    }
+
+    stage('Shell script') {
+      steps {
+        sh '''pylint $(git ls-files \'*.py\') --exit-zero'''
+      }
+    }
+  }
+}
